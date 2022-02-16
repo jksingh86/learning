d05858192e123e1c4be3b25552cd10a016e2a2fa
Shashikant Tanti <shashikant.tanti@gmail.com>
2022-02-16 19:53:48 +0530

    initial commit

---
+++ src/main/java/com/knoldus/learning/java/entity/Contact.java
@@ -0,0 +1,29 @@
+package com.knoldus.learning.java.entity;
+
+public class Contact {
+    private String mobile;
+    private String email;
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Contact(String mobile, String email) {
+        super();
+        this.mobile = mobile;
+        this.email = email;
+
+    }
+}
